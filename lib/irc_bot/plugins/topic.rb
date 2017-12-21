cc87f4c7f1940a8d8eab522c28f03de921d7fc00
Richard Degenne <richard.degenne@perfect-memory.com>
2017-12-21 14:00:55 +0100

    Migrated !topic

--- lib/irc_bot/plugins/topic.rb
+++ lib/irc_bot/plugins/topic.rb
@@ -1,38 +1,12 @@
+require_relative 'topic/generator'
+require_relative 'topic/plugin'
+
 module IRCBot
   module Plugins
-    ##
-    # A plugin that changes the topic with motivational quotes.
-    #
-    # *Syntax*
-    #
-    #     !topic
-    class Topic
-      include Cinch::Plugin
-
-      match 'topic'
-
-      ##
-      # Replies to any message matching the plugin's pattern.
-      #
-      # @param message
-      def execute(message)
-        q.first.send("SETTOPIC #{message.channel} #{topic.generate}")
-      end
-
-      private
-
-      attr_reader :topic, :bot
-
-      def topic
-        @topic ||= IRCBot::Topic::Generator.new
-      end
-
-      def q
-        @q ||= bot.channels.first.users.detect {|user, modes| user.nick=='Q'}
-      end
+    module Topic
     end
 
-    register(Topic)
+    register(Topic::Plugin)
   end
 end
 
